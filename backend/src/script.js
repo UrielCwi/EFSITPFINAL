991aaa7172f3ec9dfefacfcd34306f7412c0ea0c
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-09-30 08:48:25 -0300

    27 09 2

--- backend/src/script.js
+++ backend/src/script.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import EventController from "./controllers/EventController.js"
 import ProvinceController from "./controllers/ProvinceController.js"
 import UsersController from "./controllers/UsersController.js"
@@ -8,8 +9,8 @@ import EventCategoryController from "./controllers/EventCategoriesController.js"
 import { BDConfig } from "./BD/bd.js";
 
 const app = express();
-app.use(express.json());
-const port = 5432;
+app.use(cors());
+const port = 5000;
 
 app.use("/api/event", EventController);
 app.use("/api/province", ProvinceController);
@@ -18,7 +19,7 @@ app.use("/api/location", LocationController);
 app.use("/api/event-location", EventLocationController);
 app.use("/api/event-category", EventCategoryController);
 
-app.listen(port, ()  => 
-{console.log("Loaded")
-console.log('config', BDConfig)}
-)
+const server = app.listen(port, () => {
+    console.log(`API corriendo en http://localhost:${port}`);
+});
+server.timeout = 10000; // 10 segundos de timeout
