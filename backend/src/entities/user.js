202b42a4908b0efe6f8a704ce41d2248670867bb
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-11-22 08:48:58 -0300

    abrazo

--- backend/src/entities/user.js
+++ backend/src/entities/user.js
@@ -1,9 +1,10 @@
 export default class User{
-    constructor(id, first_name, last_name, username, password){
+    constructor(id, first_name, last_name, username, password, isAdmin){
         this.id = id
         this.first_name = first_name
         this.last_name = last_name
         this.username = username
         this.password = password
+        this.isAdmin = isAdmin
     }
 }
